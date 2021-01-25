d9f99ec4f2e71fbc1c993d07b2c097f587af410b
Leandro <lealmeids@gmail.com>
2021-01-24 20:42:33 -0400

    Realizando processo de binding

--- app/src/main/java/com/example/aluraviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/com/example/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -1,11 +1,16 @@
 package com.example.aluraviagens.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
 import com.example.aluraviagens.R;
 import com.example.aluraviagens.model.Pacote;
@@ -39,7 +44,25 @@ public class ListaPacotesAdapter extends BaseAdapter {
 
     @Override
     public View getView(int posicao, View view, ViewGroup parent) {
-        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_pacote, parent, false);
+        View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_pacote,
+                parent, false);
+        Pacote pacote = pacotes.get(posicao);
+
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem(), "drawable",
+                context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        dias.setText(pacote.getDias() + " dias");
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        preco.setText(pacote.getPreco().toString());
         return viewCriada;
     }
 }
